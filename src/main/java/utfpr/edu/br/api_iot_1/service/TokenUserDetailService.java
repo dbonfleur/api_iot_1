0019395c5d867525ea869870daf860fa7d471f8c
Mateus <mateusstamm@alunos.utfpr.edu.br>
2024-04-16 19:35:12 -0300

    Update HTTP Request Auth

--- src/main/java/utfpr/edu/br/api_iot_1/service/TokenUserDetailService.java
+++ src/main/java/utfpr/edu/br/api_iot_1/service/TokenUserDetailService.java
@@ -1,4 +1,4 @@
-package utfpr.edu.br.api_iot_1.service;
+/*package utfpr.edu.br.api_iot_1.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,4 +16,31 @@ public class TokenUserDetailService implements UserDetailsService {
         return (UserDetails) pessoaService.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
+}*/
+
+package utfpr.edu.br.api_iot_1.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import utfpr.edu.br.api_iot_1.model.Pessoa;
+
+@Component
+public class TokenUserDetailService implements UserDetailsService {
+    @Autowired
+    private PessoaService pessoaService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Pessoa pessoa = pessoaService.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                pessoa.getEmail(), pessoa.getSenha(), new ArrayList<>()
+        );
+    }
 }
+
